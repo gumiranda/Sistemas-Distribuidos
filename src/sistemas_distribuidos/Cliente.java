e0ca004cba7e3a1365cc9ea6c42c7974f3e94634
ALexandre Pereira Marcos <alexandrepm2810@gmail.com>
2019-04-24 14:49:33 -0300

    Cliente com arquivo

--- src/sistemas_distribuidos/Cliente.java
+++ src/sistemas_distribuidos/Cliente.java
@@ -12,19 +12,32 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 
 public class Cliente{
     private int porta;
     private String host;
-    
     public Cliente(String host,int porta){
         this.porta = porta;
         this.host = host;
     }
      public static void main(String[] args) throws IOException{
-       new Cliente("127.0.0.1", 1234).executa();
+        String nome_arquivo = args[0];
+        String ip;
+        int porta;
+        BufferedReader br = new BufferedReader(new FileReader(nome_arquivo));
+        String linha = br.readLine();
+	br.close(); 
+        String esp[] = linha.split(" ");
+        ip = esp[0];
+        porta =  Integer.valueOf(esp[1]);
+         new Cliente(ip,porta).executa();
+       
+    
     }
+       
     
     public void executa() throws IOException{
         Socket cliente = new Socket(this.host,this.porta);
